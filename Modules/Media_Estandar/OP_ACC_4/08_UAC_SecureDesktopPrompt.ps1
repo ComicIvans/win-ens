b4018d72e0385dda9509b48d8ba2d2951b9696f3
Iván <isalidocobo@gmail.com>
2025-08-09 21:17:43 +0200

    Add missing policies for OP_ACC_4 group

---
+++ Modules/Media_Estandar/OP_ACC_4/08_UAC_SecureDesktopPrompt.ps1
@@ -0,0 +1,26 @@
+﻿###############################################################################
+# 08_UAC_SecureDesktopPrompt.ps1
+# Control de cuentas de usuario: cambiar al escritorio seguro cuando se pida
+# confirmación de elevación
+###############################################################################
+
+# Object with policy's execution information
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+$PolicyInfo = [PSCustomObject]@{
+  Name   = '08_UAC_SecureDesktopPrompt'
+  Status = 'Pending'
+}
+
+# Object with policy's metadata
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+$PolicyMeta = [PSCustomObject]@{
+  Name             = '08_UAC_SecureDesktopPrompt'
+  Description      = 'Control de cuentas de usuario: cambiar al escritorio seguro cuando se pida confirmación de elevación'
+  Type             = 'Registry'
+  Path             = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System'
+  Property         = 'PromptOnSecureDesktop'
+  ExpectedValue    = 1
+  ValueKind        = 'DWord'
+  ComparisonMethod = 'AllowedValues'
+  AllowedValues    = @(1)
+}
---
+++ Modules/Media_Estandar/OP_ACC_4/09_DC_RefuseMachinePwdChange.ps1
@@ -0,0 +1,25 @@
+﻿###############################################################################
+# 09_DC_RefuseMachinePwdChange.ps1
+# Controlador de dominio: no permitir los cambios de contraseña de cuenta de equipo
+###############################################################################
+
+# Object with policy's execution information
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+$PolicyInfo = [PSCustomObject]@{
+  Name   = '09_DC_RefuseMachinePwdChange'
+  Status = 'Pending'
+}
+
+# Object with policy's metadata
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+$PolicyMeta = [PSCustomObject]@{
+  Name             = '09_DC_RefuseMachinePwdChange'
+  Description      = 'Controlador de dominio: no permitir los cambios de contraseña de cuenta de equipo'
+  Type             = 'Registry'
+  Path             = 'HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters'
+  Property         = 'RefusePasswordChange'
+  ExpectedValue    = 0
+  ValueKind        = 'DWord'
+  ComparisonMethod = 'AllowedValues'
+  AllowedValues    = @(1)
+}
