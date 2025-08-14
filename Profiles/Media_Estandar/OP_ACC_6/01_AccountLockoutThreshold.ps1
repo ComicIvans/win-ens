1d967582262c0a71bb9144cbc58466973548f768
Iván <isalidocobo@gmail.com>
2025-08-14 14:47:53 +0200

    Add OP_ACC_6 group

---
+++ Profiles/Media_Estandar/OP_ACC_6/01_AccountLockoutThreshold.ps1
@@ -0,0 +1,15 @@
+﻿###############################################################################
+# 01_AccountLockoutThreshold.ps1
+# Umbral de bloqueo de cuenta
+###############################################################################
+
+# Object with policy's metadata
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+$PolicyMeta = [PSCustomObject]@{
+  Name             = '01_AccountLockoutThreshold'
+  Description      = 'Umbral de bloqueo de cuenta'
+  Type             = 'Security'
+  Property         = 'LockoutBadCount'
+  ExpectedValue    = 5
+  ComparisonMethod = 'LessOrEqual'
+}
---
+++ Profiles/Media_Estandar/OP_ACC_6/02_AccountLockoutDuration.ps1
@@ -0,0 +1,21 @@
+﻿###############################################################################
+# 02_AccountLockoutDuration.ps1
+# Duración del bloqueo de cuenta
+#
+# AVISO: Esta política debe aplicarse para también pueda que la siguiente,
+# 03_AccountLockoutResetTime. Debe utilizarse un valor de -1 o uno mayor
+# o igual al de la siguiente.
+#
+###############################################################################
+
+# Object with policy's metadata
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+$PolicyMeta = [PSCustomObject]@{
+  Name             = '02_AccountLockoutDuration'
+  Description      = 'Duración del bloqueo de cuenta'
+  Type             = 'Security'
+  Property         = 'LockoutDuration'
+  ExpectedValue    = -1
+  ComparisonMethod = 'AllowedValues'
+  AllowedValues    = @(-1)
+}
